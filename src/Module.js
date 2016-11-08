447fa64a049132a32c46fc1cff7c7199dff14180
Sarah Ganter <sa.ganter@web.de>
2016-11-08 17:53:31 +0100

    Include jquery and add changeStatus-method (not working yet)

--- src/Module.js
+++ src/Module.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import $ from 'jquery';
 
 class Module extends React.Component {
+
+  changeStatus() {
+    $('#sem01_mod01').mouseOver(function() {
+      $(this).css('background-color', 'red');
+    });
+  }
+
   render() {
     return (
-      <p id="sem01_mod01" className="button">Programmierung 1</p>
+      <div>
+        <p id="sem01_mod01" className="button">Programmierung 1</p>
+      </div>
     )
   }
 }
