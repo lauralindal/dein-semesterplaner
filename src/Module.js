37cbc731d7e1ce0d10b36579cf129f18bcba8e1e
Sarah Ganter <sa.ganter@web.de>
2016-11-18 21:48:42 +0100

    remove multiple id

--- src/Module.js
+++ src/Module.js
@@ -4,7 +4,7 @@ class Module extends React.Component {
   render() {
     return (
       <div key={this.props.module.title} className="semester">
-          <p id="sem01_mod01" className="button hover" >{this.props.module.title}</p>
+          <p className="button hover" >{this.props.module.title}</p>
       </div>
     )
   }
