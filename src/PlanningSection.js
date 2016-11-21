5fc5bb28137f726f78427fe616e9b2b5d27519b6
Sarah Ganter <sa.ganter@web.de>
2016-11-21 15:12:34 +0100

    Add section-wrapper for PlanningSection

--- src/PlanningSection.js
+++ src/PlanningSection.js
@@ -5,17 +5,19 @@ import CurrentCredits from './CurrentCredits';
 class PlanningSection extends React.Component {
   render() {
     return (
-      <div id="planningSection">
-        <div id="sum" className="float-left">
-          <div id="sum_total">
-            <h5>Alle Leistungspunkte</h5>
-            Du hast bisher [xxx] Leistungspunkte gesammelt.
+      <section id="planningSection">
+        <div id="planningSection">
+          <div id="sum" className="float-left">
+            <div id="sum_total">
+              <h5>Alle Leistungspunkte</h5>
+              Du hast bisher [xxx] Leistungspunkte gesammelt.
+            </div>
+            <CurrentCredits />
           </div>
-          <CurrentCredits />
+          <ModulePool />
+          <p className="u-cf"></p>
         </div>
-        <ModulePool />
-        <p className="u-cf"></p>
-      </div>
+      </section>
     )
   }
 }
