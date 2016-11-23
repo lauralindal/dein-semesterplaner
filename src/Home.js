2120033db961db78069ba9079e641e35b40dfe1c
lauralindal <lauralaugwitz@gmail.com>
2016-11-23 20:58:26 +0100

    Create Header Component

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,7 @@ class Home extends React.Component {
       });
     return (
       <div>
+        <Login />
         <ModulePlan semesters={semesters}/>
         <PlanningSection />
       </div>
