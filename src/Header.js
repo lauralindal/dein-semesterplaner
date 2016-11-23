ae0b973a74d8a782859777f40983b585bf7ab562
lauralindal <lauralaugwitz@gmail.com>
2016-11-23 21:49:14 +0100

    Add Login Fields

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,23 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>Dein Semesterplaner</h1>
+        <form>
+          <div className="row">
+            <h1 className="four columns">Dein Semesterplaner</h1>
+            <div className="three columns">
+              <label htmlFor="Username">Username</label>
+              <input type="text" placeholder="Username" id="Username" />
+            </div>
+            <div className="three columns">
+              <label htmlFor="Username">Password</label>
+              <input type="password" placeholder="Password" id="Password" />
+            </div>
+            <div className="two">
+              <label htmlFor="exampleEmailInput">&nbsp;</label>
+              <input className="button-primary" type="submit" value="Login" />
+            </div>
+          </div>
+        </form>
       </header>
     )
   }
