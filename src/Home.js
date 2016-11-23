2ab02566b029e7190324ac7203bf0a7c4a21735d
Sarah Ganter <sa.ganter@web.de>
2016-11-23 22:34:05 +0100

    display status on button

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,23 @@ import users from './users.json';
 
 class Home extends React.Component {
   render() {
+    var userModules = users.students[0].tracked_modules;
     var modules = moduleplan.degree_course.modules;
     var maxSemester = 6;
     var semesters = [1,2,3,4,5,6].map(function(semester) {
-      return modules.filter(function(module) {
+      var filteredModules = modules.filter(function(module) {
         return module.recommended_semester === semester;
-        });
       });
+      return filteredModules.map(function(module) {
+        var userModule = userModules.find(function(userModule) {
+          return userModule.module_id === module.id;
+        })
+        return {
+           module: module,
+           userModule: userModule
+        }
+      })
+    });
     return (
       <div>
         <ModulePlan semesters={semesters}/>
@@ -23,3 +33,47 @@ class Home extends React.Component {
 }
 
 export default Home
+
+// semesters = [
+//   [
+//     {
+//       "module": {
+//         "id": 1,
+//         "title": "Programmierung 1",
+//         "description": "Kara",
+//         "cp": 6,
+//         "prerequisite": false,
+//         "recommended_semester": 1
+//       },
+//       "userModule": {
+//         "status": "urgent"
+//       }
+//     },
+//     {
+//       "id": 2,
+//       "title": "Grundlegende Konzepte der Informatik",
+//       "description": "Hackhack",
+//       "cp": 5,
+//       "prerequisite": false,
+//       "recommended_semester": 1
+//     }
+//   ],
+//   [
+//     {
+//       "id": 1,
+//       "title": "Programmierung 1",
+//       "description": "Kara",
+//       "cp": 6,
+//       "prerequisite": false,
+//       "recommended_semester": 2
+//     },
+//     {
+//       "id": 2,
+//       "title": "Grundlegende Konzepte der Informatik",
+//       "description": "Hackhack",
+//       "cp": 5,
+//       "prerequisite": false,
+//       "recommended_semester": 2
+//     }
+//   ]
+// ]
