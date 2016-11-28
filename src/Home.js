1b20c9e56f46f1274f0e66cc647c2c179c44127b
Sarah Ganter <sa.ganter@web.de>
2016-11-28 11:43:38 +0100

    tighten the render method

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,18 @@ import users from './users.json';
 
 class Home extends React.Component {
   render() {
+    var semesters = this.getSemestersForUser();
+    return (
+      <div>
+        <ModulePlan semesters={semesters}/>
+        <PlanningSection />
+      </div>
+    );
+  }
+
+   getSemestersForUser() {
     var userModules = users.students[0].tracked_modules;
     var modules = moduleplan.degree_course.modules;
-    var maxSemester = 6;
     var semesters = [1,2,3,4,5,6].map(function(semester) {
       var filteredModules = modules.filter(function(module) {
         return module.recommended_semester === semester;
@@ -16,19 +25,14 @@ class Home extends React.Component {
       return filteredModules.map(function(module) {
         var userModule = userModules.find(function(userModule) {
           return userModule.module_id === module.id;
-        })
+        });
         return {
            module: module,
            userModule: userModule
         }
-      })
+      });
     });
-    return (
-      <div>
-        <ModulePlan semesters={semesters}/>
-        <PlanningSection />
-      </div>
-    )
+    return semesters;
   }
 }
 
