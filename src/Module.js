ad4e609b0a322c7f3bce0a15a456cfde8a08fa4a
Sarah Ganter <sa.ganter@web.de>
2016-11-30 21:05:31 +0100

    Display different colours for status

--- src/Module.js
+++ src/Module.js
@@ -2,11 +2,36 @@ import React from 'react';
 
 class Module extends React.Component {
   render() {
+    var ModuleClasses = "button hover";
+
+    switch (this.props.userModule.status) {
+      case "urgent":
+        ModuleClasses += " red";
+        break;
+      case "started":
+        ModuleClasses += " yellow";
+        break;
+      case "selected":
+        ModuleClasses += " green";
+        break;
+      case "completed":
+        ModuleClasses += " grey";
+        break;
+      default:
+        ModuleClasses += " blue";
+    }
+
     return (
       <div key={this.props.module.title} className="semester">
-          <p className="button hover" >{this.props.module.title} {this.props.userModule.status}</p>
+          <p className={ModuleClasses} onClick={this.addStatusColor.bind(this)}>{this.props.module.title} {this.props.userModule.status}</p>
       </div>
     )
+  }
+
+  addStatusColor() {
+    if (this.props.userModule.status === "urgent") {
+      <p className="button hover red" >{this.props.module.cp} {this.props.userModule.status}</p>
+    }
   }
 }
 
