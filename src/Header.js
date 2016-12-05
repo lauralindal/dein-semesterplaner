516f150c1649feaf073dbb6a5aa81f19eeac1719
lauralindal <lauralaugwitz@gmail.com>
2016-12-05 22:11:53 +0100

    Conditional rendering of Login component or button, depending on status

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Login from './Login';
 
 class Header extends React.Component {
+
   render() {
+    const isLoggedIn = true;
     return (
       <header>
-        <div className="row">
-          <h1 className="four columns">Dein Semesterplaner</h1>
+      <div className="row">
+        <h1 className="four columns">Dein Semesterplaner</h1>
+        {isLoggedIn ? (
+          <button className="button-primary" type="button" value="Logout" />
+        ) : (
           <Login performLogin={this.props.performLogin} />
-        </div>
+        )}
+      </div>
       </header>
     )
   }
