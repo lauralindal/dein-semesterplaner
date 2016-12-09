5e06f8d565b6ed7acc24c48ed7d237a39f130cc8
noobiba <noobanine@gmail.com>
2016-12-09 10:35:26 +0100

    Transform course schedule into React component.

---
+++ src/CourseSchedule.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+class CourseSchedule extends React.Component {
+  render() {
+    return (
+      
+      <section id="schedule">
+      <h5>Kalender/Weekly Planner</h5>
+    <table>
+      <tr>
+        <th>Zeiten</th>
+        <th>Montag</th>
+        <th>Dienstag</th>
+        <th>Mittwoch</th>
+        <th>Donnerstag</th>
+        <th>Freitag</th>
+        <th>Samstag</th>
+      </tr>
+      <tr>
+        <td>8.00 - 9.30</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>9.45 - 11.15</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>12.15 - 13.45</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>14.00 - 15.30</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>15.45 - 17.15</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+    </section>
+
+
+    )
+  }
+}
+
+export default CourseSchedule
