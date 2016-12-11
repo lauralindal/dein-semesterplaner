5e877204734aab98634ec8e5b08a0867b60a1c62
noobiba <noobanine@gmail.com>
2016-12-11 20:50:36 +0100

    Align text with element.

--- src/ModulePlan.js
+++ src/ModulePlan.js
@@ -12,7 +12,7 @@ class ModulePlan extends React.Component {
 
         <legend>
           <div className="flex-container">
-            <div class="flex-item item1">
+            <div className="flex-item item1">
               <div className="available"></div>
               <div><h6>Kurs belegt</h6></div>
             </div>
@@ -36,46 +36,14 @@ class ModulePlan extends React.Component {
               <div className="selected_urgent"></div>
               <div><h6>Dringlichkeitskurs geplant</h6></div>
             </div>
-
- 
-{/*
-          <div className="float-left">
-          
           </div>
-
-          <div className="float-left">
-          <div className="started"></div>
-          <div>Angefangener<br/>Kurs</div>
-          </div>
-
-          <div className="float-left">
-          <div className="completed"></div>
-          <div>Abgeschlossener<br/>Kurs</div>
-          </div>
-
-          <div className="float-left">
-          <div className="urgent"></div>
-          <div>Wiederholbarkeitsfrist<br/>läuft ab</div>
-          </div>
-
-          <div className="float-left">
-          <div className="selected"></div>
-          <div>Für das aktuelle Semester<br/>eingeplanter Kurs</div>
-          </div>
-
-          <div className="float-left">
-          <div className="selected_urgent"></div>
-          <div>Dringend abzuschliessender<br/> Kurs</div>
-          </div>
-*/}
-          </div>
-
-
-      </legend>
+        </legend>
+      
       </div>
 
-      </section>
-      <div>
+    </section>
+      
+    <div>
       Regelstudienplan
       {this.props.semesters.map(function(semester, index) {
         return (
