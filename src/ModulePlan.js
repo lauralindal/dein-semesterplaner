222d81ec51f560818ed5114deb34ae98af663e65
noobiba <noobanine@gmail.com>
2016-12-11 20:30:46 +0100

    Change wording for legend.

--- src/ModulePlan.js
+++ src/ModulePlan.js
@@ -14,27 +14,27 @@ class ModulePlan extends React.Component {
           <div className="flex-container">
             <div class="flex-item item1">
               <div className="available"></div>
-              <div><h6>Belegbarer<br/>Kurs</h6></div>
+              <div><h6>Kurs belegt</h6></div>
             </div>
             <div className="flex-item item2">
               <div className="started"></div>
-              <div><h6>Angefangener<br/>Kurs</h6></div>
+              <div><h6>Kurs begonnen</h6></div>
             </div>
             <div className="flex-item item3">
               <div className="completed"></div>
-              <div><h6>Abgeschlossener<br/>Kurs</h6></div>
+              <div><h6>Kurs beendet</h6></div>
             </div>
             <div className="flex-item item4">
               <div className="urgent"></div>
-              <div><h6>Wiederholbarkeitsfrist<br/>läuft ab</h6></div>
+              <div><h6>Kurs mit Dringlichkeit</h6></div>
             </div>
             <div className="flex-item item5">
               <div className="selected"></div>
-              <div><h6>Für das aktuelle Semester<br/>eingeplanter Kurs</h6></div>
+              <div><h6>Kurs geplant</h6></div>
             </div>
             <div className="flex-item item6">
               <div className="selected_urgent"></div>
-              <div><h6>Dringend abzuschliessender<br/> Kurs</h6></div>
+              <div><h6>Dringlichkeitskurs geplant</h6></div>
             </div>
 
  
