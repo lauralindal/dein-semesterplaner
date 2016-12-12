1a359482df78eb3045be3cd9ca081d53ae2124cb
noobiba <noobanine@gmail.com>
2016-12-12 14:04:59 +0100

    Further trying.

---
+++ src/Weekday.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Weekday extends React.Component {
+  render() {
+    return (
+      <div>
+      Bananenkuchen
+      Wochentag 1
+      </div>
+    )
+  }
+}
+
+export default Weekday
