969abe9212d9d8344eff86b35b10de0c4756db42
noobiba <noobanine@gmail.com>
2016-12-12 13:48:34 +0100

    Create Weekday component. Trying to display it.

--- src/CourseSchedule.js
+++ src/CourseSchedule.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Weekday from './Weekday';
 
 class CourseSchedule extends React.Component {
   render() {
     return (
-      
       <section id="schedule">
-      <h5>Kalender/Weekly Planner</h5>
-    <table>
-    <tbody>
+
+    <h5>Kalender/Weekly Planner</h5>
+
+<table>
+    <thead>
       <tr>
         <th>Zeiten</th>
         <th>Montag</th>
@@ -17,57 +19,65 @@ class CourseSchedule extends React.Component {
         <th>Freitag</th>
         <th>Samstag</th>
       </tr>
+    </thead>  
+    <tbody>
       <tr>
-        <td>8.00 - 9.30</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>9.45 - 11.15</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>12.15 - 13.45</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>14.00 - 15.30</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>15.45 - 17.15</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      </tbody>
-    </table>
-    </section>
+       <td>8.00 - 9.30</td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+     </tr>
+     <tr>
+       <td>9.45 - 11.15</td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+     </tr>
+     <tr>
+       <td>12.15 - 13.45</td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+     </tr>
+     <tr>
+       <td>14.00 - 15.30</td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+     </tr>
+     <tr>
+       <td>15.45 - 17.15</td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+     </tr>
+     </tbody>
+   </table>
+   <br />
+   
+<Weekday />
+
+  
+</section>
 
 
-    )
+      )
+    
   }
 }
 
