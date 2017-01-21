d6212f2ed055e1f91ee8b4f200e047dbe75f2f39
noobiba <noobanine@gmail.com>
2017-01-21 17:25:10 +0100

    Display selected courses in time slots.

--- src/CourseSchedule.js
+++ src/CourseSchedule.js
@@ -4,66 +4,7 @@ import TimeSlots from './TimeSlots';
 
 class CourseSchedule extends React.Component {
 
-  
-createTimeSlots(){
-  const starttimes= ["8:00","9:45","12:15","14:00","15:45"];
-  const days= ["Montag","Dienstag", "Mittwoch","Donnerstag","Freitag","Samstag" ];
-  var counter= 0;
-  var slots= [];
-    for (var i = 0; i < starttimes.length; i++) {
-      for (var j = 0; j < days.length; j++) {
-        slots.push({id:counter, day: days[j], time:starttimes[i] , info:null})
-        counter++;
-      }
-    }
-  return slots;
-};
-
-fillTimeSlots(){
-  var courseInfo= this.props.combinedTitleAndData;
-  var timeslots= this.createTimeSlots();
-  if (typeof courseInfo != 'undefined'){
-    for (var i = 0; i < timeslots.length; i++) {
-      for (var j = 0; j < courseInfo.length; j++) {
-        if (timeslots[i].day === courseInfo[j].day && timeslots[i].time === courseInfo[j].startTime)
-        {
-          timeslots[i].info=courseInfo[j];
-        }
-      }
-    }
-  }
-  return timeslots;
-};
-
- 
-/*getTimeSlots() {
-  const sessions= [
-  { id:1, starttime:'8.00', day:'Monday', name:'slot 1'},
-  { id:2, starttime:'9.45', name:'slot 2'},
-  { id:3, starttime:'12.15', name:'slot 3'},
-  { id:4, starttime:'14.00', name:'slot 4'},
-  { id:5, starttime:'15.45', name:'slot 5'}
-  ];
-  return sessions.map((slot) => {
-    return(
-      <td> time={slot.starttime} name={slot.name} key={slot.id} </td>
-      );
-  });
-}
-
-renderTimeSlots() {
-  return timeslots.map(slot)=> {
-    return(
-      <td> title={slot.info.title} session={slot.info.course-format} instructor={slot.info.course-format} key={slot.id} </td>
-      );
-  });
-};
-*/
-
-
 render() {
-    var filledTimeSlots= this.fillTimeSlots();
-
     return (
     <section id="schedule">
       <h5>Kalender/Weekly Planner</h5>
@@ -79,7 +20,7 @@ render() {
         <th>Samstag</th>
       </tr>
     </thead>
-        <TimeSlots />
+        <TimeSlots combinedTitleAndData={this.props.combinedTitleAndData}/>
     </table>
     </section>
     )
--- src/TimeSlots.js
+++ src/TimeSlots.js
@@ -2,57 +2,45 @@ import React from 'react';
 
 class TimeSlots extends React.Component {
 
+  renderTimes(){
+    const starttimes=["8:00","9:45","12:15","14:00","15:45"];
+    var _this= this;
+    return starttimes.map(function(time){
+      return (<tr key={time}> {_this.renderSlots(time)} </tr>);
+    });
+  };
+
+  renderSlots(time){
+    const days = [null,"Montag","Dienstag", "Mittwoch","Donnerstag","Freitag","Samstag"];
+    var _this= this;
+    return days.map(function(day){
+      return (<td key={time+day}> {_this.renderCell(time, day)} </td>);
+    });
+  };
+
+  renderCell(time, day) {
+    if (day===null){
+      return (<div>{time}</div>);
+    }
+    else{
+      var courseInfo= this.props.combinedTitleAndData;
+      for (var j = 0; j < courseInfo.length; j++) {
+        if (day === courseInfo[j].day && time === courseInfo[j].startTime)
+        {
+           return (<div> {courseInfo[j].title} {courseInfo[j]["course-format"]} {courseInfo[j].lecturer} </div>);
+        }
+      }
+    }
+  };
+
 
   render() {
+
     return (
      <tbody>
-     <tr>
-      <td>8.30 - 9.45</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>9.45 - 11.15</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>12.15 - 13.45</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>14.00 - 15.30</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>15.45 - 17.15</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
+     {this.renderTimes()}
      </tbody>
-    )
+    );
   }
 }
 
