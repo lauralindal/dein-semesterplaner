18cf500fcfc6827d192dfc38dbabeb03bb942563
lauralindal <lauralaugwitz@gmail.com>
2017-01-25 10:36:29 +0100

    Provide method to calculate remaining study semesters

--- src/CurrentCredits.js
+++ src/CurrentCredits.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 class CurrentCredits extends React.Component {
+
+
   render() {
     return (
       <div id="sum_current">
-        <h5>Aktuelle Leistungspunkte</h5>
-          Du hast in diesem Semester {this.props.selectedCoursesCounter} Module mit insgesamt {this.props.currentCreditPoints} Leistungspunkten belegt.
+      <h5>Aktuelle Leistungspunkte</h5>
+      Du hast in diesem Semester {this.props.selectedCoursesCounter} Module mit insgesamt {this.props.currentCreditPoints} Leistungspunkten belegt.
+      <h5>Studiendauer</h5>
+      Basierend auf der Annahme, dass du durchschnittlich 30 Leistungspunkte pro Semester erwirbst, wirst du nach dem kommenden Semester noch {this.props.remainingSemesters} Semester studieren.
       </div>
     )
   }
