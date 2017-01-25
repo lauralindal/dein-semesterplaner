6316e790bb6f20840267512f7b312378f77c4e00
lauralindal <lauralaugwitz@gmail.com>
2017-01-25 10:42:47 +0100

    Adjust styling for display of remaining semester information

--- src/CurrentCredits.js
+++ src/CurrentCredits.js
@@ -7,9 +7,8 @@ class CurrentCredits extends React.Component {
     return (
       <div id="sum_current">
       <h5>Aktuelle Leistungspunkte</h5>
-      Du hast in diesem Semester {this.props.selectedCoursesCounter} Module mit insgesamt {this.props.currentCreditPoints} Leistungspunkten belegt.
-      <h5>Studiendauer</h5>
-      Basierend auf der Annahme, dass du durchschnittlich 30 Leistungspunkte pro Semester erwirbst, wirst du nach dem kommenden Semester noch {this.props.remainingSemesters} Semester studieren.
+      <p>Du hast in diesem Semester {this.props.selectedCoursesCounter} Module mit insgesamt {this.props.currentCreditPoints} Leistungspunkten belegt.</p>
+      <p>Basierend auf der Annahme, dass du durchschnittlich 30 Leistungspunkte pro Semester erwirbst, wirst du nach dem kommenden Semester noch {this.props.remainingSemesters} Semester studieren.</p>
       </div>
     )
   }
