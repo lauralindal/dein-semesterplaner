7c42cb387bfc6bcaf23ee2dd99e11286fb956561
Lena Koester <lenakoester@Jarets-MBP.fritz.box>
2017-01-29 15:15:27 +0100

    adjust text in currenrCredits if only 1 module is selected

--- src/CurrentCredits.js
+++ src/CurrentCredits.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class CurrentCredits extends React.Component {
+
+  createText(){
+  	var numberOfModules= this.props.selectedCoursesCounter;
+  	if (numberOfModules===1){
+  		return "Modul";
+  	}else{
+  		return "Module";
+  	}
+  }
+
   render() {
     return (
       <div id="sum_current">
         <h5>Aktuelle Leistungspunkte</h5>
-          Du hast in diesem Semester {this.props.selectedCoursesCounter} Module mit insgesamt {this.props.currentCreditPoints} Leistungspunkten belegt.
+          Du hast in diesem Semester {this.props.selectedCoursesCounter} {this.createText()} mit insgesamt {this.props.currentCreditPoints} Leistungspunkten belegt.
       </div>
     )
   }
